3d94cd72fff2869dc47a8fe21bc187820f9b7233
aaqifshafi <aaqifshafi@gmail.com>
2022-12-04 00:18:13 +0530

    error fixed

--- js/script.js
+++ js/script.js
@@ -7,16 +7,6 @@ function closeMenu() {
     sidemen.style.right = "-200px";
 }
 
-
-window.addEventListener('scroll', () => {
-    const sectionPos = skill.getBoundingClientRect().top;
-    const screenPos = window.innerHeight;
-    if (sectionPos > screenPos) {
-        showProgress();
-    }
-    else {
-    }
-});
 // ---------Typing effect---------
 var typed = new Typed(".typing", {
     strings: ["", "Student", "Web-Developer", "Freelancer"],
